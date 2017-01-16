36a0e487be0b6b9688977e0442ced0d24837c67f
Alex Robertson <lex1989@gmail.com>
2017-01-16 17:34:44 +0000

    Homepage now has nested ui views

--- js/app.js
+++ js/app.js
@@ -8,6 +8,17 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: 'views/partial-home.html'
     })
+    .state('home.list', {
+      url: '/list',
+      templateUrl: 'views/partial-home-list.html',
+      controller: function($scope) {
+        $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+      }
+    })
+    .state('home.paragraph', {
+      url: '/paragraph',
+      template: 'I could sure use a drink right now.'
+    })
     .state('about', {
       url: '/about',
       templateUrl: 'views/partial-about.html'
